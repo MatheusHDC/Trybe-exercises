defd473ba4e523e2d82507d8eb84f9b1b28f5d24
Matheus Domingos <matheus.h1323@gmail.com>
2023-03-29 10:58:38 -0300

    terminando atividades pre aula

---
+++ DIa1/aividade1.js
@@ -0,0 +1,7 @@
+let myName="Matheus";
+let birthCity="Nova União";
+let birthYear=2003;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ atividade3.js
@@ -0,0 +1,8 @@
+let nota = 45;
+if (nota >= 80){
+    console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
+}else if (80 > nota && nota >= 60){
+    console.log('Você está na nossa lista de espera.');
+}else {
+    console.log('Infelizmente, você reprovou.')
+}
