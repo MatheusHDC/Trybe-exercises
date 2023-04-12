0a45bcfa004d1875f470dd641feb45e82332f159
Matheus Domingos <matheus.h1323@gmail.com>
2023-04-12 13:11:10 -0300

    comecando exercicios pos aula

---
+++ script.js
@@ -0,0 +1,7 @@
+const h1 = document.createElement('h1');
+h1.innerText = 'TrybeTrip - Agência de Viagens';
+document.body.appendChild(h1);
+
+const mainContent = document.createElement('main');
+mainContent.className = 'main-content';
+document.body.appendChild(mainContent);
