e4388c94b61e26d2c2696218d3984fb24d8fa2e7
Matheus Domingos <matheus.h1323@gmail.com>
2023-04-12 13:22:37 -0300

    exercicios 3 a 6 ok

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ document.body.appendChild(h1);
 const mainContent = document.createElement('main');
 mainContent.className = 'main-content';
 document.body.appendChild(mainContent);
+
+const centerContent = document.createElement('section');
+centerContent.className = 'center-content';
+mainContent.appendChild(centerContent);
+
+const frase = document.createElement('p');
+frase.innerText = 'Texto sobre agencia';
+centerContent.appendChild(frase);
+
+const leftContent = document.createElement('section');
+leftContent.className = 'left-content';
+mainContent.appendChild(leftContent);
+
+const rightContent = document.createElement('section');
+rightContent.className = 'right-content';
+mainContent.appendChild(rightContent);
+
