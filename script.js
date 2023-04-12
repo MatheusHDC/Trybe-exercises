48a09474f4298781c7da7a13deca77a1d05c8c04
Matheus Domingos <matheus.h1323@gmail.com>
2023-04-12 13:46:23 -0300

    termindos exercicios de 1 a 9

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ const rightContent = document.createElement('section');
 rightContent.className = 'right-content';
 mainContent.appendChild(rightContent);
 
+const imagem = document.createElement('img');
+imagem.src = 'https://picsum.photos/200';
+imagem.className = 'small-image';
+leftContent.appendChild(imagem);
+
+const listaNaoOrdenada = document.createElement('ul');
+rightContent.appendChild(listaNaoOrdenada);
+
+const criaLista = () =>{
+    for(let valor = 1; valor <= 10; valor += 1){
+        const lista = document.createElement('li');
+        lista.innerHTML = `${valor}`;
+        listaNaoOrdenada.appendChild(lista);
+    };
+};
+criaLista();
+
+for(let index = 0; index < 3; index += 1){
+    const h3 = document.createElement('h3');
+    h3.innertext = index;
+    mainContent.appendChild(h3);
+};
