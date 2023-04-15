23dca8136c9d69a1e6ffc6854a7a37d0080350a5
Matheus Domingos <matheus.h1323@gmail.com>
2023-04-15 12:51:21 -0300

    troca cor de fundo e troca cor da fonte

--- Dia4/script1.js
+++ Dia4/script1.js
@@ -1,4 +1,5 @@
 const body = document.querySelector('body');
+const TextoALer = document.getElementById('texto-a-ler');
 const backgroundColors = document.querySelectorAll('#background-color button');
 const fontColor = document.querySelectorAll('#font-color button');
 const fontSize = document.querySelectorAll('#font-size button');
@@ -12,4 +13,14 @@ const trocaFundo = () =>{
         });
     };
 };
+const corDaFonte = () =>{
+    for(let elementos of fontColor){
+        elementos.addEventListener('click', ()=>{
+            body.style.color = elementos.innerText;
+            TextoALer.style.borderColor = elementos.innerText; 
+        });
+    };
+};
+
 trocaFundo();
+corDaFonte();
